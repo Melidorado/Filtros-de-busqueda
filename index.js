4cd4a8035e7420db870100754493b84694d5d3cb
Melidorado <meliidorado@hotmail.com>
2020-09-26 13:55:01 -0300

    Codigo clase 29

--- index.js
+++ index.js
@@ -1,31 +1,65 @@
 const cards = document.querySelectorAll(".card")
-const botones = document.querySelectorAll(".boton-color")
+const filtroColores = document.querySelectorAll("input[type='checkbox']")
+
 
 const sexos = document.querySelectorAll("input[name='sexo']")
-console.log(sexos)
+
 
 const busqueda = document.getElementById("filtro-escrito")
 
-console.log(busqueda)
 
 
 
-for (let boton of botones) {
-    boton.onclick = () => {
-        for (let card of cards) {
-            
-            if (boton.dataset.color === card.dataset.color) {
+/* for (let color of filtroColores) {
+    color.onclick = () => {
+      filtrarPorColores()
+    }
+}
+
+filtrarPorColores = () => {
+    for (let card of cards) {
+        card.classList.add("hidden")
+        if (colorSeleccionado()) {
+            if (mismosColores(card)){
                 card.classList.remove("hidden")
             }
-            else if (boton.dataset.color === "todos") {
+            if (todosColores()) {
                 card.classList.remove("hidden")
             }
-            else {
-                card.classList.add("hidden")
-            }
+
+        }
+        else {
+            card.classList.remove("hidden")
         }
     }
 }
+
+colorSeleccionado = () => {
+    for (let color of filtroColores) {
+        if (color.checked) {
+            return true
+        }
+    }
+}
+
+mismosColores = (card) => {
+    for (let color of filtroColores) {
+        if (color.value === card.dataset.color && color.checked) {
+            return true
+        }
+    }
+}
+
+todosColores = () => {
+    for (let color of filtroColores) {
+        if (color.value === "todos" && color.checked) {
+            return true
+        }
+    }
+}
+
+
+
 
 
 for (let sexo of sexos) {
@@ -57,3 +91,127 @@ busqueda.oninput = () => {
         }
     }
 }
+ */
+
+
+/*-------------------- CODIGO NUEVO ----------------------------*/
+
+
+
+busqueda.oninput = () => { 
+    filtrarTarjetas()
+}
+
+for (let radio of sexos) {
+    radio.oninput = () => {
+        filtrarTarjetas()
+    }
+}
+
+for (let checkbox of filtroColores) {
+    checkbox.oninput = () => {
+      filtrarTarjetas()
+    }
+}
+
+const filtrarTarjetas = () => {
+    for (let card of cards) {
+        if (pasaFiltros(card)) {
+            mostrarTarjetas(card)
+        }
+        else {
+            ocultarTarjetas(card)
+        }
+    }
+}
+
+
+const mostrarTarjetas = (card) => {
+    return card.classList.remove("hidden")
+}
+
+const ocultarTarjetas = (card) => {
+    return card.classList.add("hidden")
+}
+
+const hayAlgunCheckboxChequeado = () => {
+    for (let checkbox of filtroColores) {
+        if (checkbox.checked) {
+            return true
+        }
+    }
+    return false
+}
+
+const hayAlgunRadioSeleccionado = () => {
+    for (let radio of sexos) {
+        if (radio.checked) {
+            return true
+        }
+    }
+    return false
+}
+
+const hayAlgoEscritoEnInput = () => {
+    return Boolean(busqueda.value)
+}
+
+
+const coincideBusquedaInputConTarjeta = (card) => {
+    const nombreTarjeta = card.dataset.nombre
+    const busquedaUsuario = busqueda.value.toLowerCase()
+    
+    if (nombreTarjeta.includes(busquedaUsuario)) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+
+const pasaFiltros = (card) => {
+
+  /*   if (hayAlgoEscritoEnInput()) {
+        return coincideBusquedaInputConTarjeta(card)
+    }
+    else {
+        return true
+    } */
+
+  /*   if (hayAlgunCheckboxChequeado()) {
+        for (let checkbox of filtroColores) {
+            if (checkbox.value === card.dataset.color && checkbox.checked) {
+                return true
+            }
+            if (checkbox.value === "todos" && checkbox.checked) {
+                return true
+            }
+        }
+        return false
+    }
+    else {
+        return true
+    } */
+
+    if (hayAlgunRadioSeleccionado()) {
+        for (let radio of sexos) {
+            if (radio.value === card.dataset.sexo && radio.checked) {
+                return true
+            }
+           
+        }
+        return false
+    }
+    else {
+        return true
+    }
+
+
+
+
+}
+
+
+
+ 
